e6cd5dce3b4f83122d018f96a2c874e005fdd3e8
gnurullayev <nurullayevgayrat73@gmail.com>
2022-10-12 12:35:01 +0500

    aside componente change

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -3,7 +3,8 @@ import "./aside.css"
 import search from "../../assets/search.svg"
 import asideSetings from "../../assets/aside-setinges.svg"
 import trendsSettingsImg from "../../assets/trends-setings-img.png"
-
+import avatar1 from "../../assets/avatar1.png"
+import avatar2 from "../../assets/avater2.png"
 function Aside() {
     return (
         <aside className='aside'>
@@ -64,6 +65,56 @@ function Aside() {
                     <div className="trends__footer">
                         <a className='trends__footer-btn' href="/">Show more</a>
                     </div>
+                </div>
+
+                <div className="aside__likes likes">
+                    <div className="likes__header">
+                        <p className='likes__header-text'>Trends for you</p>
+                    </div>
+
+                    <div className="likes__body">
+                        <ul className="likes__list">
+                            <li className='likes__item'>
+                                <img className='likes__img' src={avatar1} alt="profile" />
+
+                                <div className="likes__content">
+                                    <h3 className="likes__title">Mushtariy</h3>
+                                    <h5 className="likes__subtitle">@Mushtar565266</h5>
+                                </div>
+
+                                <button className='likes__btn'>
+                                    Follow
+                                </button>
+                            </li>
+
+                            <li className='likes__item'>
+                                <img className='likes__img' src={avatar2} alt="profile" />
+
+                                <div className="likes__content">
+                                    <h3 className="likes__title">Mushtariy</h3>
+                                    <h5 className="likes__subtitle">@Mushtar565266</h5>
+                                </div>
+
+                                <button className='likes__btn'>
+                                    Follow
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="likes__footer">
+                        <a className='likes__footer-btn' href="/">Show more</a>
+                    </div>
+                </div>
+                   
+                <div className="aside__end">
+                    <a className='aside__end-link' href="/">Terms of Service</a>
+                    <a className='aside__end-link' href="/">Privacy Policy</a>
+                    <a className='aside__end-link' href="/">Cookie Policy</a>
+                    <a className='aside__end-link' href="/">Imprint</a>
+                    <a className='aside__end-link' href="/">Ads Info</a>
+                    <a className='aside__end-link' href="/">More ···</a>
+                    <a className='aside__end-link' href="/">© 2021 Twitter, Inc.</a>
                 </div>
             </div>
         </aside>
